e3aab6805eff8689cdf6a590425ab8ed959e8ce8
Carlos Ranara <carlosranara@Carloss-MacBook-Air.local>
2024-10-19 13:43:58 +0800

    done

---
+++ conditionnotes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int x, y, z;
+
+    
+    scanf("%d%d%d", &x, &y, &z);
+
+
+    if (x > y && x > z) {
+        printf("The highest value is: %d\n", x);
+    } else if (y > z) {
+        printf("The highest value is: %d\n", y);
+    } else {
+        printf("The highest value is: %d\n", z);
+    }
+
+    return 0;
+}
