07656252ac05d6ca0a01d70c82a4f0170b59a6dc
CalloLinux <carlosranara0@gmail.com>
2024-10-30 16:56:56 +0800

    practice done i wish it didnt take me that long

---
+++ robot-func.c
@@ -0,0 +1,48 @@
+
+int hasEnergy(int energy){
+if (energy > 0)
+	return 1;
+else
+	return 0;
+
+}
+
+int getAction(int action) {
+	switch (action){
+		case 1: return -3;
+		case 2: return -7;
+		case 3: return 3;
+		default: {
+			printf("Not a valid command! Action wasted!\n");
+			return 0;
+		}
+	}
+}
+
+void useEnergy(int action, int *energy) {
+int i = getAction(action);
+
+	if (hasEnergy(*energy) == 1)
+	{	
+		if (*energy >= action)
+			{
+			*energy += i;
+			if (action == 1)
+				printf("The robot walked! Energy reduced by 3!\n");
+				
+ 			else if (action == 2)
+				printf("The robot fired a shot! Energy reduced by 7!\n");
+			
+			else
+				printf("The robot recharged some energy! Energy raised by 3!");
+			}
+		else
+			printf("Not enough energy!\n");
+	}
+	
+	else {
+		printf("No more energy! \n Recharging... Energy raised by 3\n");
+		*energy += getAction(3);
+		}
+
+}
